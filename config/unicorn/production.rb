ce41d66073646b82534625895b0bd539cb329787
meganii <yuhei24@gmail.com>
2017-02-06 23:00:46 +0900

    Add unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -11,3 +11,26 @@ pid "#{@dir}tmp/pids/unicorn.pid"
 
 stderr_path "#{@dir}log/unicorn.stderr.log"
 stdout_path "#{@dir}log/unicorn.stdout.log"
+
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |_server, _worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
