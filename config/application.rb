7647455d61960b13ca19267cc82b55dd2de187ca
meganii <yuhei24@gmail.com>
2017-02-06 22:59:36 +0900

    Add config

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Kuroko2App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Asia/Tokyo'
+    config.autoload_paths << Rails.root.join('lib', 'autoload')
+    config.eager_load_paths << Rails.root.join('lib', 'autoload')
   end
 end
